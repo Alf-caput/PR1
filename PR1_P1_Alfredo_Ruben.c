2ff284f2fcbe4ca61bd6b1d17589f225bfe30975
Alf-caput <alfre.roble.aba@gmail.com>
2022-11-20 15:11:17 +0100

    Inicio ejercicio 1

---
+++ PR1_P1_Alfredo_Ruben.c
@@ -0,0 +1,12 @@
+/*=============================================================================
+ |   Asignatura:  Estructura de datos y algoritmos
+ |      Autores:  Alfredo Robledano Abasolo, Ruben Sierra Serrano
+ |     Lenguaje:  C
+ |        Clase:  2-B
+ |        Fecha:  20-11-2022
+ +-----------------------------------------------------------------------------
+ |  Descripcion:  Lectura de fichero con geolocalizaciones y almacenamiento
+ |                en conjuntos para posteriormente realizar operaciones
+ |      Entrada:  Fichero csv con latitudes y longitudes, operacion a realizar
+ |       Salida:  Resultado de operar sobre los conjuntos obtenidos
+ *===========================================================================*/
